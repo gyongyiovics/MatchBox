1b4098a96a1d2f1ba7391d9fb17d1095e10ccc71
gyongyiovics <gyongyimako@gmail.com>
2021-10-09 18:13:22 +0200

    added method to GameField

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import com.example.demo.gaming.GameField;
+
 public class Main {
     public static void main(String[] args) {
-
+        GameField gameField = new GameField();
+        gameField.play();
     }
 }
--- src/main/java/com/example/demo/gaming/GameField.java
+++ src/main/java/com/example/demo/gaming/GameField.java
@@ -1,4 +1,38 @@
 package com.example.demo.gaming;
 
+import com.example.demo.helpers.GameHelper;
+import com.example.demo.models.Computer;
+import com.example.demo.models.StupidComputer;
+
+import java.util.Scanner;
+
 public class GameField {
+
+    public void play() {
+        Scanner scanner = new Scanner(System.in);
+        Computer enemy = new StupidComputer();
+        int matches = GameHelper.random(4, 40);
+
+        do {
+            System.out.println("Matches on the table: " + matches);
+            System.out.println("Take 1-3 matches!");
+            int playerMatches = scanner.nextInt();
+            matches -= playerMatches;
+
+            if(matches == 1) {
+                System.out.println("You won!");
+                break;
+            }
+
+            int enemyMatches = enemy.takeMatches(matches);
+            matches -= enemyMatches;
+
+            System.out.println("Computer took " + enemyMatches + " matches.");
+
+            if(matches == 1) {
+                System.out.println("Computer won!");
+                break;
+            }
+        } while (true);
+    }
 }
